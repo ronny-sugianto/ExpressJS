96b878e419e0e2b55fcd1182d2fe129daac6b6d3
ronny-sugianto <ronnysugianto.id@gmail.com>
2020-05-13 11:17:04 +0700

    [ADD] add file .env

--- app-db.js
+++ app-db.js
@@ -2,13 +2,14 @@ import Sequlize from 'sequelize';
 import dotenv from 'dotenv';
 
 
+dotenv.config();
 const conn = new Sequlize(
-    "gomux",
-    "root",
-    "postgres", {
-    dialect: "postgres",
-    host: "127.0.0.1",
-    port: "5432"
+    process.env.DB_NAME,
+    process.env.DB_USERNAME,
+    process.env.DB_PASSWORD, {
+    dialect: process.env.DB_TYPE,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT
 }
 );
 
--- app-main.js
+++ app-main.js
@@ -4,13 +4,16 @@ import conn from './app-db';
 
 
 dotenv.config();
-
-conn.authenticate().then(() => {
-    server.listen(9000, '0.0.0.0', function () {
-        if (server.listening) {
-            console.log('APP-INFO [SERVER] : Server is listening ');
-        }
+if (process.env.APP_NAME) {
+    conn.authenticate().then(() => {
+        server.listen(process.env.APP_PORT ?? 3898, '0.0.0.0', function () {
+            if (server.listening) {
+                console.log('APP-INFO [SERVER] : Server is listening ');
+            }
+        });
+    }).catch((err) => {
+        console.log('APP-ERROR [DB-FAILED] : ' + err);
     });
-}).catch((err) => {
-    console.log('APP-ERROR [DB-FAILED] : ' + err);
-});
+} else {
+    process.exit(1);
+}
